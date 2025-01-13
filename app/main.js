fdc5327fb7fcc5c1878adedde4c5d2212ce2440e
Beatriz Avila <beatriz.heav@gmail.com>
2025-01-13 13:36:25 -0600

    Container margin added

--- app/main.js
+++ app/main.js
@@ -1,11 +1,12 @@
 import React from "react";
 import CustomTitle from "../components/CustomTitle";
 import { View } from "react-native";
+import { containers } from "../styles/containers";
 
 const Main = () => {
   return (
-    <View>
-      <CustomTitle text={"Titulo grande"} type={"big"} />,
+    <View style={containers.main}>
+      <CustomTitle text={"Titulo grande"} type={"big"} />
       <CustomTitle text={"Titulo chico"} type={"small"} />
     </View>
   );
