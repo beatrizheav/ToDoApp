9d9af565c8adeb559241fbb04ff4427113ed7d6c
Beatriz Avila <beatriz.heav@gmail.com>
2025-01-15 13:05:08 -0600

    Main file edited

--- app/main.js
+++ app/main.js
@@ -5,43 +5,11 @@ import InputDatePicker from "../components/InputDatePicker";
 import CustomInput from "../components/CustomInput";
 
 const Main = () => {
-  const [name, onChangeName] = useState("");
-  const [email, onChangeEmail] = useState("");
-  const [password, onChangePassword] = useState("");
-  const [paragraph, onChangeParagraph] = useState("");
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState();
 
   return (
     <View style={containers.main}>
       <InputDatePicker label={"Due Date"} date={date} setDate={setDate} />
-      <CustomInput
-        type={"text"}
-        label={"Name"}
-        placeholder={"Ingresa tu nombre"}
-        value={name}
-        onChangeValue={onChangeName}
-      />
-      <CustomInput
-        type={"email"}
-        label={"Email address"}
-        placeholder={"example@gmail.com"}
-        value={email}
-        onChangeValue={onChangeEmail}
-      />
-      <CustomInput
-        type={"password"}
-        label={"Password"}
-        placeholder={"Enter password"}
-        value={password}
-        onChangeValue={onChangePassword}
-      />
-      <CustomInput
-        type={"paragraph"}
-        label={"Description"}
-        placeholder={"Add a descripion"}
-        value={paragraph}
-        onChangeValue={onChangeParagraph}
-      />
     </View>
   );
 };
