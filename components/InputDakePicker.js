f5e76910bb8e225c71b9e4fe0224101fb48c77e1
Beatriz Avila <beatriz.heav@gmail.com>
2025-01-15 10:47:32 -0600

    react-native-date-picker removed and @react-native-community/datepicker added

--- components/InputDakePicker.js
+++ components/InputDakePicker.js
@@ -1,8 +1,37 @@
-import { Text } from "react-native";
-import { fontsTheme } from "../styles/fontsTheme";
+// import { Text } from "react-native";
+// import { fontsTheme } from "../styles/fontsTheme";
 
-const InputDatePicker = ({}) => {
-  return <Text>Hola</Text>;
+// const InputDatePicker = ({}) => {
+//   return <Text>Hola</Text>;
+// };
+
+// export default InputDatePicker;
+
+import React, { useState } from "react";
+import { Button } from "react-native";
+import DatePicker from "react-native-date-picker";
+
+const InputDatePicker = () => {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Button title="Open" onPress={() => setOpen(true)} />
+      {/* <DatePicker
+        modal
+        open={open}
+        date={date}
+        onConfirm={(date) => {
+          setOpen(false);
+          setDate(date);
+        }}
+        onCancel={() => {
+          setOpen(false);
+        }}
+      /> */}
+    </>
+  );
 };
 
 export default InputDatePicker;
