083264ce31f1b909500bb026a96b543ee5423a57
Beatriz Avila <beatriz.heav@gmail.com>
2025-01-21 13:47:31 -0600

    Splash screen added

--- app/index.js
+++ app/index.js
@@ -1,31 +1,34 @@
 import { useEffect } from "react";
 import { useRouter } from "expo-router";
-import { LogBox } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 import {
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
-import { OpenSans_600SemiBold } from "@expo-google-fonts/open-sans";
-import { Manrope_700Bold } from "@expo-google-fonts/manrope";
 
-LogBox.ignoreAllLogs(true);
+SplashScreen.preventAutoHideAsync();
 
-export default function Index() {
+export default function App() {
   const router = useRouter();
 
-  let [fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    OpenSans_600SemiBold,
-    Manrope_700Bold,
   });
 
   useEffect(() => {
     if (fontsLoaded) {
+      SplashScreen.hideAsync();
       router.replace("/main");
     }
   }, [fontsLoaded, router]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return null;
 }
